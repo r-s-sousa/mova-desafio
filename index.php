587cdaf4fb5f0020655d6b82f5b4211b09e9d581
= <rafael_sousa2018@outlook.com>
2021-11-08 05:10:43 -0300

    finish challenge with dates

--- index.php
+++ index.php
@@ -7,12 +7,28 @@ use Rafael\Mova\Show;
 
 require __DIR__."/vendor/autoload.php";
 
+$dates = [
+   1 => 'Domingo',
+   2 => 'Segunda-feira',
+   3 => 'Terça-feira',
+   4 => 'Quarta-feira',
+   5 => 'Quinta-feira',
+   6 => 'Sexta-feira',
+   7 => 'Sábado',
+];
+
+$events = [
+   1 => '',
+   2 => 'da Proclamação da república',
+   3 => 'dos Finados',
+   4 => '',
+   5 => 'das crianças',
+   6 => 'de codificar :)',
+   7 => ''
+];
+
 $obDayWeek = new DayWeek();
 $obShow = new Show($obDayWeek);
-$obShow->setDates([1 => 'Segunda-feira']);
-$obShow->setEvents([1 => 'finados']);
+$obShow->setDates($dates);
+$obShow->setEvents($events);
 $obShow->showEventsWeek();
-
-echo "<pre>";
-print_r($obShow);   
-echo "</pre>";
--- src/DayWeek.php
+++ src/DayWeek.php
@@ -16,7 +16,31 @@ final class DayWeek implements IDayWeek
     * @param array $events array com data do mês e evento
     * @return void
     */
-   public function ShowEvents(array $dates, array $events) {
-      echo 'teste';
+   public function ShowEvents(array $dates, array $events)
+   {
+      foreach ($dates as $date => $day) {
+         echo $this->GetMessageDay($date, $day, $events[$date]);
+         echo "<br>";
+      }
+   }
+
+   /**
+    * Função responsável por retornar o texto da mensagem de acorco com a 
+    * observação do referido dia
+    *
+    * @param integer $dayNum
+    * @param integer $dayName
+    * @param string $obs
+    * @return void
+    */
+   private function GetMessageDay(int $dayNum, string $dayName, string $obs): string
+   {
+      $dayNum == 1 ? $dayNum = "1º" : $dayNum;
+      $msnStart = "Hoje é dia {$dayNum}, $dayName";
+
+      if(strlen($obs) == 0)
+         return $msnStart;
+
+      return $msnStart." e também é dia {$obs}";
    }
 }
